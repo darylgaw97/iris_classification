c975dcd5bcf233b3e61e23a095ed341a85acdb66
Daryl <85093569+darylgaw97@users.noreply.github.com>
2022-06-29 21:49:04 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -27,7 +27,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = pd.read_csv('https://raw.githubusercontent.com/darylgaw97/iris_classification/main/Iris.csv')
-X = iris.data
+X = iris['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
 Y = iris.target
 
 clf = RandomForestClassifier()
